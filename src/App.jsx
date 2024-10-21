24dfbdfa745b392013a5a4ca53f0dedc13d085d8
Abdukadir001 <abdukodir374@gmail.com>
2024-10-21 17:13:59 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import About from './components/About/About'
+import Card from './components/Card/Card'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Wrapper from './components/Wrapper/Wrapper'
+
+const App = () => {
+  return (
+    <>
+    <About/>
+    <Card/>
+    <Footer/>
+    <Header/>
+    <Hero/>
+    <Wrapper/>
+    
+    </>
+  )
+}
+
+export default App
